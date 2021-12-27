622212c507681d4d3fd9a83275775f4c59aa6182
Phuc Le <lelocminhphuc@gmail.com>
2021-12-27 17:23:00 +0100

    Finish lab 5

--- Lab5_FunctionsInC/main.c
+++ Lab5_FunctionsInC/main.c
@@ -46,6 +46,13 @@ unsigned long Calc_Area(unsigned long l, unsigned long w) {
   unsigned long result;
 
 // Put your Lab 5 code here
-  
+  if ((l<3) || (w<3) || (l>20) || (w>20))
+	{
+		result = 0;
+	}
+	else
+	{
+		result = l*w;
+	}
   return(result);
 }
