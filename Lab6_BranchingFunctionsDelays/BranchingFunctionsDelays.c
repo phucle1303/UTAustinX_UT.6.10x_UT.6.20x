4862be68aab35e72a9245667c71b3debb54e1e1e
Phuc Le <lelocminhphuc@gmail.com>
2021-12-28 18:33:25 +0100

    Finish lab 6

--- Lab6_BranchingFunctionsDelays/BranchingFunctionsDelays.c
+++ Lab6_BranchingFunctionsDelays/BranchingFunctionsDelays.c
@@ -29,12 +29,47 @@
 void DisableInterrupts(void); // Disable interrupts
 void EnableInterrupts(void);  // Enable interrupts
 
+// delay function
+void Delay100ms(unsigned long time); 
+
 int main(void){ unsigned long volatile delay;
   TExaS_Init(SW_PIN_PF4, LED_PIN_PF2);  // activate grader and set system clock to 80 MHz
   // initialization goes here
+	SYSCTL_RCGC2_R |= SYSCTL_RCGC2_GPIOF; 													 // activate clock for port F
+	delay = SYSCTL_RCGC2_R;               												   // allow time for clock to start
+	GPIO_PORTF_AMSEL_R &= ~0x14;          												   // clear the PF4 and PF2 bits in Port F AMSEL to disable analog
+	GPIO_PORTF_PCTL_R &= ~0x000F0F00;     												   // clear the PF4 and PF2 bit fields in Port F PCTL to configure as GPIO
+	GPIO_PORTF_DIR_R = (GPIO_PORTF_DIR_R&(~0x10))|0x04;;             // PF4 is an input and PF2 is an output
+	GPIO_PORTF_AFSEL_R &= ~0x14;          													 // clear the PF4 and PF2 bits in Port F AFSEL to disable alternate functions
+	GPIO_PORTF_DEN_R |= 0x14;																				 // set the PF4 and PF2 bits in Port F DEN to enable digital
+	GPIO_PORTF_PUR_R |= 0x10;             													 // set the PF4 bit in Port F PUR to activate an internal pullup resistor
+	GPIO_PORTF_DATA_R |= 0x04;																			 // set the PF2 bit in Port F DATA so the LED is initially ON
 
   EnableInterrupts();           // enable interrupts for the grader
   while(1){
     // body goes here
+		Delay100ms(1);
+		if (!(GPIO_PORTF_DATA_R&0x10))
+		{
+			GPIO_PORTF_DATA_R ^= 0x04;
+		}
+		else
+		{
+			GPIO_PORTF_DATA_R |= 0x04;
+		}
+  }
+}
+
+void Delay100ms(unsigned long time)
+{
+  unsigned long i;
+  while(time > 0)
+	{
+    i = 1333333;  // this number means 100ms
+    while(i > 0)
+		{
+      i = i - 1;
+    }
+    time = time - 1; // decrements every 100 ms
   }
 }
