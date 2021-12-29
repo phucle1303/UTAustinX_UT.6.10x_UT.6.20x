f23861b7653c9819299cb1e2bb80a76355f86a40
Phuc Le <lelocminhphuc@gmail.com>
2021-12-29 20:35:26 +0100

    Finish lab 8

--- Lab8_SwitchLEDinterface/SwitchLEDInterface.c
+++ Lab8_SwitchLEDinterface/SwitchLEDInterface.c
@@ -17,6 +17,8 @@
 // FUNCTION PROTOTYPES: Each subroutine defined
 void DisableInterrupts(void); // Disable interrupts
 void EnableInterrupts(void);  // Enable interrupts
+void PortE_Init(void);
+void Delay1ms(unsigned long msec);
 
 // ***** 3. Subroutines Section *****
 
@@ -30,11 +32,42 @@ int main(void){
 // The following version tests input on PE0 and output on PE1
 //**********************************************************************
   TExaS_Init(SW_PIN_PE0, LED_PIN_PE1, ScopeOn);  // activate grader and set system clock to 80 MHz
-  
+  PortE_Init();
+	
 	
   EnableInterrupts();           // enable interrupts for the grader
   while(1){
-    
+		GPIO_PORTE_DATA_R |= 0x02;
+		while (GPIO_PORTE_DATA_R&0x01)
+		{
+			Delay1ms(100);
+			GPIO_PORTE_DATA_R ^= 0x02;
+		}
   }
   
+}
+
+void PortE_Init(void){ volatile unsigned long delay;
+  SYSCTL_RCGC2_R |= 0x10;      			 // 1) E clock
+  delay = SYSCTL_RCGC2_R;            // delay to allow clock to stabilize     
+  GPIO_PORTE_AMSEL_R &= 0x00;        // 2) disable analog function
+  GPIO_PORTE_PCTL_R &= 0x00000000;   // 3) GPIO clear bit PCTL  
+  GPIO_PORTE_DIR_R &= ~0x01;         // 4.1) PE0 input,
+  GPIO_PORTE_DIR_R |= 0x02;          // 4.2) PE1 output  
+  GPIO_PORTE_AFSEL_R &= 0x00;        // 5) no alternate function    
+  GPIO_PORTE_DEN_R |= 0x03;          // 6) enable digital pins PF4-PF1
+}
+
+void Delay1ms(unsigned long msec){
+// write this function
+	unsigned long i;
+  while(msec > 0)
+	{
+    i = 13333;  // this number means 1ms
+    while(i > 0)
+		{
+      i = i - 1;
+    }
+    msec = msec - 1; // decrements every 1 ms
+  }
 }
